d34da72bc1e92618d7b8e72b599225a3bd406bd0
unknown <teodor.popa96@gmail.com>
2023-10-12 22:20:42 +0300

    Update 12.10

--- style.js
+++ style.js
@@ -1,1 +1,14 @@
+/*const name = "Popa Teodor";
+const yearOfBirth = 1996;
 
+console.log(`${name} was born in ${yearOfBirth}.`); */
+
+// Block scoping
+
+{
+  const name = "P. Teodor";
+  console.log(name);
+  {
+    console.log(name);
+  }
+}
