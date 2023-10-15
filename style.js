addd13783a91525b2c837c6412e5b78c63d04001
unknown <teodor.popa96@gmail.com>
2023-10-15 17:59:53 +0300

    JS Functions

--- style.js
+++ style.js
@@ -12,3 +12,32 @@ console.log(`${name} was born in ${yearOfBirth}.`); */
     console.log(name);
   }
 }
+
+// Formatting and Linting - Code formatting
+
+const name = "Teodor!";
+let age = 27;
+console.log(`My name is ${name} and I am ${age} years old`);
+
+// Linting
+
+const first = "test";
+const second = "test";
+if (first === second) {
+  console.log("Hello!");
+}
+
+// Defining and using functions
+
+const hello = () => {
+  console.log("Hello everyone!");
+};
+hello();
+
+// Function parameters
+
+const greet = (name) => {
+  console.log(`Hello ${name}!`);
+};
+
+greet("Teodor");
