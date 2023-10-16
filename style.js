c51f5235e77a4573271b93a77ed7fead58e02ec7
unknown <teodor.popa96@gmail.com>
2023-10-16 22:27:36 +0300

    Update Maps and Sets

--- style.js
+++ style.js
@@ -176,3 +176,19 @@ console.log(uniques);
 uniques.delete("two");
 console.log(uniques);
 console.log(uniques.has("two"));
+
+// Iteration
+
+const countryNames = new Map();
+countryNames.set("FIN", "Finland");
+countryNames.set("SWE", "Sweden");
+for (const [key, value] of countryNames) {
+  console.log(`${key} -> ${value}`);
+}
+console.log("---");
+const countries = new Set();
+countries.add("Finland");
+countries.add("Sweden");
+for (const value of countries) {
+  console.log(value);
+}
