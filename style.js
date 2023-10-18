8e643b8af67c4aa57f1fcea3bed5c4263a3e301e
unknown <teodor.popa96@gmail.com>
2023-10-18 20:02:00 +0300

    Asynchronous Functions

--- style.js
+++ style.js
@@ -223,3 +223,14 @@ const obj = JSON.parse(jsonString);
 console.log(obj.name);
 const string = JSON.stringify(obj);
 console.log(string);
+
+// Asynchronous Functions
+// Defining asynchronous functions
+
+const waitAndPrint = async (message, seconds) => {
+  await new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+  console.log(message);
+};
+waitAndPrint("First call!", 3);
+waitAndPrint("Second call!", 2);
+waitAndPrint("Third call!", 1);
