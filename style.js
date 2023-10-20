1a3fb5c7f57874c57b3865686d094535ba1fdc8d
unknown <teodor.popa96@gmail.com>
2023-10-20 21:21:54 +0300

    Update functions

--- style.js
+++ style.js
@@ -227,11 +227,21 @@ console.log(string);
 // Asynchronous Functions
 // Defining asynchronous functions
 
-const waitAndPrint = async (message, seconds) => {
+/*const waitAndPrint = async (message, seconds) => {
   await new Promise((resolve) => setTimeout(resolve, seconds * 1000));
   console.log(message);
 };
 waitAndPrint("First call!", 3);
+waitAndPrint("Second call!", 2);
+waitAndPrint("Third call!", 1);*/
+
+// Waiting for an asynchronous function
+
+const waitAndPrint = async (message, seconds) => {
+  await new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+  console.log(message);
+};
+await waitAndPrint("First call!", 3);
 waitAndPrint("Second call!", 2);
 waitAndPrint("Third call!", 1);
 
